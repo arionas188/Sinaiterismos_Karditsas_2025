5dee518ce152f5a600ee4cc4c2f4b43d0532ac70
arionas188 <arionaskonstantinos@me.com>
2025-06-21 18:24:26 +0300

    update upirseies

--- index.js
+++ index.js
@@ -20,3 +20,28 @@ document.querySelectorAll('.toggle-link').forEach(link => {
       }
     });
   });
+
+// annimation sto id=animatedHeading stis upiresies section3
+const heading = document.getElementById('animatedHeading');
+function playAnimation() {
+    // Αφαιρούμε την κλάση (reset)
+    heading.classList.remove('animate__pulse'); 
+
+    // Αναγκάζουμε reflow για να ξαναπαίξει σωστά
+    void heading.offsetWidth;
+
+    // Ξαναπροσθέτουμε την κλάση
+    heading.classList.add('animate__pulse');
+
+    // Επαναφορά μετά από 3 δευτερόλεπτα
+    setTimeout(playAnimation, 2000);
+  }
+  // Έναρξη animation loop όταν φορτωθεί το DOM
+  document.addEventListener("DOMContentLoaded", playAnimation);
+
+  setInterval(() => {
+    const badge = document.getElementById('neo1-badge');
+    badge.classList.remove('animate__animated', 'animate__flash');
+    void badge.offsetWidth;
+    badge.classList.add('animate__animated', 'animate__flash');
+  }, 2000);
